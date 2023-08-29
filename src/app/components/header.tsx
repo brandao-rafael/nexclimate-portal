57c0b0d7760a0d138a3f51e31a274a7d31e3e867
brandaobrandisborges <rafaelbrandao1992@gmail.com>
2023-08-29 12:06:05 -0300

    feat: create header

---
+++ src/app/components/header.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Header: React.FC = () => {
+  return (
+    <div className='flex justify-between p-8 bg-cyan-500'>
+      <h1>NexClimate Portal</h1>
+      <div className='w-1/5 flex justify-between'>
+        <a href="/">Climate</a>
+        <a href="/postalcode">Postal code</a>
+        <a href="/weather">Contact</a>
+      </div>
+    </div>
+  )
+}
+
+export default Header
---
+++ src/app/pages/climate.page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Header from '../components/header';
+
+const ClimatePage: React.FC = () => {
+  return (
+    <div>
+      <Header />
+    </div>
+  )
+}
+
+export default ClimatePage;
