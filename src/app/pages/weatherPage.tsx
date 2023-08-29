915df991862e12d057deb1575e5b68d029a531b5
brandaobrandisborges <rafaelbrandao1992@gmail.com>
2023-08-29 17:34:29 -0300

    feat: initial structure for weather page

--- src/app/pages/weatherPage.tsx
+++ src/app/pages/weatherPage.tsx
@@ -1,7 +1,11 @@
 import React, { useEffect } from 'react'
+import Image from 'next/image'
 import userWheather from '../api/userWheather'
 import WeatherData from '../interfaces/weatherData'
 import Header from '../components/header'
+import RainIcon from '../../../public/images/rain-icon.png'
+
+const DAYS_OF_WEEK = 7;
 
 const WeatherPage: React.FC = () => {
   const [weatherData, setWeatherData] = React.useState<WeatherData | null>(null)
@@ -32,27 +36,55 @@ const WeatherPage: React.FC = () => {
       <Header />
       <div className="p-0 bg-cover h-screen" style={backgroundStyles}>
         {weatherData ? (
-          <div>
-            <div className="flex justify-center items-center h-full w-1/3">
-              <div className="bg-white rounded-lg shadow-lg p-8">
-                <h1 className="text-gray-900 text-3xl font-light">
+          <div className="flex">
+            <div className="flex justify-center items-center h-screen w-1/3">
+              <div className="bg-white/25 rounded-lg shadow-lg p-8">
+                <h1 className="text-black text-3xl font-light">
                   {weatherData.city}
                 </h1>
-                <h2 className="text-gray-700 text-3xl font-light">
+                <h2 className="text-gray-900 text-3xl font-light">
                   {weatherData.temp}°C
                 </h2>
-                <h2 className="text-gray-600 text-xl">
+                <h2 className="text-gray-700 text-xl">
                   {weatherData.description}
                 </h2>
-                <p className="text-gray-600">
-                  {weatherData.humidity}% humidity
+                <p className="text-gray-700">
+                  {weatherData.humidity}% Humidade
                 </p>
-                <p className="text-gray-600">
-                  {weatherData.wind_speedy} km/h wind
+                <p className="text-gray-700">
+                  {weatherData.wind_speedy}
                 </p>
               </div>
             </div>
-            <div></div>
+            <div className="flex justify-evenly h-screen w-full">
+              {weatherData.forecast.map((item, i) => i < DAYS_OF_WEEK && (
+                <div key={item.weekday + i} className="flex flex-col justify-between text-center bg-white/25 rounded-lg shadow-lg p-2 h-1/4 mt-auto mb-auto">
+                  <h1 className="text-black text-2xl font-light">
+                    {item.weekday}
+                  </h1>
+                  <p className="text-gray-600 font-light text-sm">
+                    {item.date}
+                  </p>
+                  <p className="text-gray-900 font-light text-sm">
+                    Max: {item.max}°C
+                  </p>
+                  <p className="text-gray-900 font-light text-sm">
+                    Min: {item.min}°C
+                  </p>
+                  <div className='flex justify-evenly p-1'>
+                    <Image
+                      src={RainIcon}
+                      width={32}
+                      height={32}
+                      alt="rain icon"
+                    />
+                    <h2 className="text-gray-700 text-lg">
+                      {item.rain_probability}%
+                    </h2>
+                  </div>
+                </div>
+              ))}
+            </div>
           </div>
         ) : (
           <div className="flex justify-center items-center h-full">
