ccc50de3b632498faca33545578ffa0072617372
brandaobrandisborges <rafaelbrandao1992@gmail.com>
2023-08-30 01:34:39 -0300

    styles: for header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -3,9 +3,9 @@ import React from 'react';
 
 const Header: React.FC = () => {
   return (
-    <header className='fixed top-0 flex justify-between p-8 bg-cyan-500/25 w-screen'>
-      <h1>NexClimate Portal</h1>
-      <div className='w-1/5 flex justify-between'>
+    <header className='fixed top-0 flex justify-between p-8 bg-black/50 w-screen'>
+      <h1 className='text-white font-bold text-2xl'>NexClimate Portal</h1>
+      <div className='w-1/5 flex justify-between text-white'>
         <Link href="/">Clima</Link>
         <Link href="/postalCode">Busca CEP</Link>
         <Link href="/contact">Contato</Link>
