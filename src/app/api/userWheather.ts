04593b8601fe6fb17dff6b70a80f6aaf00bf7695
brandaobrandisborges <rafaelbrandao1992@gmail.com>
2023-08-30 14:01:52 -0300

    comment

--- src/app/api/userWheather.ts
+++ src/app/api/userWheather.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const apiKey = process.env.HG_API_KEY || "7e05297e" // api key exposta, porém não é um problema, pois é uma api gratuita e não possui dados sensíveis
+const apiKey = process.env.HG_API_KEY || "7e05297e" // api key exposta, porém não é um problema, pois é uma api gratuita e não possui dados sensíveis além de possuir regras no CORS
 
 const userWheather = async () => {
   return axios
