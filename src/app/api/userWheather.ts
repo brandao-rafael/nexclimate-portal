0cc98f3725d2dc7d65fe4e51f66d7e4126e80ceb
brandaobrandisborges <rafaelbrandao1992@gmail.com>
2023-08-30 12:36:50 -0300

    fix: api key missing

--- src/app/api/userWheather.ts
+++ src/app/api/userWheather.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const apiKey = process.env.HG_API_KEY
+const apiKey = process.env.HG_API_KEY || "7e05297e" // api key exposta, porém não é um problema, pois é uma api gratuita e não possui dados sensíveis
 
 const userWheather = async () => {
   return axios
