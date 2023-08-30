4cc5939e885bb1a68c44201fb3ff367252d89944
brandaobrandisborges <rafaelbrandao1992@gmail.com>
2023-08-29 23:05:09 -0300

    refactor: semantic header

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 
 const Header: React.FC = () => {
   return (
-    <div className='fixed flex justify-between p-8 bg-cyan-500/25 w-screen'>
+    <header className='fixed top-0 flex justify-between p-8 bg-cyan-500/25 w-screen'>
       <h1>NexClimate Portal</h1>
       <div className='w-1/5 flex justify-between'>
         <Link href="/">Clima</Link>
         <Link href="/postalCode">Busca CEP</Link>
         <Link href="/contact">Contato</Link>
       </div>
-    </div>
+    </header>
   )
 }
 
