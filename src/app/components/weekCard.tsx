fc9c5079265cc76bb52deedcf3d51960c06abf04
brandaobrandisborges <rafaelbrandao1992@gmail.com>
2023-08-31 16:51:26 -0300

    style: add icon to min max temperature

--- src/app/components/weekCard.tsx
+++ src/app/components/weekCard.tsx
@@ -30,9 +30,19 @@ const WeekCard: React.FC<ItemForecast> = ({ item, weatherIconPath }) => {
     >
       <h1 className="text-white text-2xl font-light">{item.weekday}</h1>
       <Image src={weatherIconPath} width={64} height={64} alt="weather icon" />
-      <p className="text-white/50 font-light text-sm">{item.date}</p>
-      <p className="text-white/75 font-light text-sm">Max: {item.max}°C</p>
-      <p className="text-white/75 font-light text-sm">Min: {item.min}°C</p>
+      <p className="text-white/75 font-light text-sm">{item.date}</p>
+      <div className='flex justify-around mt-1'>
+        <Image src="/icons/max.png" alt="max temp" width={16} height={16}/>
+        <p className="text-white/75 font-light text-sm">
+          {item.max}°C
+        </p>
+      </div>
+      <div className='flex justify-around mt-1'>
+        <Image src="/icons/min.png" alt="min temp" width={16} height={16}/>
+        <p className="text-white/75 font-light text-sm">
+          {item.min}°C
+        </p>
+      </div>
       <div className="flex justify-between p-1 mt-1">
         <Image src={RainIcon} width={32} height={32} alt="rain icon" />
         <h2 className="text-white/75 text-lg">{item.rain_probability}%</h2>
