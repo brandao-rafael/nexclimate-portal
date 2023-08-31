a49b07e758da125a77677f5c986033a6df351b15
brandaobrandisborges <rafaelbrandao1992@gmail.com>
2023-08-31 16:54:01 -0300

    style: adjust links section

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -7,7 +7,7 @@ const Header: React.FC = () => {
       <h1 className="text-white font-thin text-2xl self-center">
         NexClimate Portal
       </h1>
-      <div className="w-5/6 sm:w-1/3 flex mt-2 justify-between self-center text-white">
+      <div className="w-5/6 sm:w-1/5 flex justify-between self-center text-white">
         <Link className='font-thin hover:text-blue-300 hover:font-bold hover:animate-pulse transition-all' href="/">Clima</Link>
         <Link className='font-thin hover:text-blue-300 hover:font-bold hover:animate-pulse transition-all' href="/postalCode">Busca CEP</Link>
         <Link className='font-thin hover:text-blue-300 hover:font-bold hover:animate-pulse transition-all' href="/contact">Contato</Link>
