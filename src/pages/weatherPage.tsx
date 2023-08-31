febd966340ce5c144eba32ab15f5197e57e51eaf
brandaobrandisborges <rafaelbrandao1992@gmail.com>
2023-08-31 15:16:52 -0300

    refactor: icon logics to follow condition code

--- src/pages/weatherPage.tsx
+++ src/pages/weatherPage.tsx
@@ -4,6 +4,7 @@ import WeatherData from '../app/interfaces/weatherData'
 import Header from '../app/components/header'
 import TodayCard from '../app/components/todayCard'
 import WeekCard from '../app/components/weekCard'
+import { log } from 'console'
 
 const DAYS_OF_WEEK = 7
 
@@ -22,22 +23,27 @@ const WeatherPage: React.FC = () => {
     padding: '0',
   }
 
-  const getWeatheIcon = (condition: string) => {
-    switch (condition) {
-      case 'tempo limpo':
-        return '/icons/sun.png'
-      case 'tempo nublado':
-        console.log('nublado')
-        return '/icons/cloud.png'
-      case 'chuva':
-        return '/icons/rain.png'
-      case 'chuvas esparsas':
-        return '/icons/drizzle.png'
-      case 'tempestade':
-        return '/icons/storm.png'
-      default:
-        return '/icons/sun_cloud.png'
+  const weatherIcons: { [key: string]: number[] } = {
+    storm: [0, 1, 3, 4, 35, 37, 38, 39, 45, 47],
+    tornado: [2],
+    snow: [5, 6, 7, 10, 13, 14, 15, 16, 17, 18, 41, 42, 43, 46],
+    sun_rain: [40],
+    sun_fog: [8, 25],
+    drizzle: [9, 11, 12],
+    fog: [19, 20, 21, 22],
+    wind: [23, 24],
+    cloud: [26, 28, 44],
+    sun_cloud: [29, 30, 34],
+    sun: [27, 31, 32, 33, 36],
+  };
+  
+  const getWeatherIcon = (conditionCode: number): string => {
+    for (let icon in weatherIcons) {
+      if (weatherIcons[icon].includes(conditionCode)) {
+        return `/icons/${icon}.png`;
+      }
     }
+    return '/icons/sun_cloud.png';
   }
 
   useEffect(() => {
@@ -69,7 +75,7 @@ const WeatherPage: React.FC = () => {
       <div className="p-0 bg-cover h-full sm:h-screen" style={backgroundStyles}>
         {weatherData ? (
           <div className="flex flex-col sm:flex-row">
-            <TodayCard weatherData={weatherData} weatherIconPath={getWeatheIcon(weatherData.description.toLowerCase())}/>
+            <TodayCard weatherData={weatherData} weatherIconPath={getWeatherIcon(parseInt(weatherData.condition_code))}/>
             <div className="flex flex-col sm:flex-row mt-10 mb-2 sm:mb-0 sm:mt-0 justify-evenly sm:h-screen h-auto sm:w-full w-5/6 self-center">
               {weatherData.forecast.map(
                 (item, i) =>
