b6009fa1ae72f5429ae680c09e0fc72aeee954bd
brandaobrandisborges <rafaelbrandao1992@gmail.com>
2023-08-31 16:45:12 -0300

    style: todayCard temp position

--- src/app/components/todayCard.tsx
+++ src/app/components/todayCard.tsx
@@ -14,10 +14,13 @@ const TodayCard: React.FC<TodayCardProps> = ({ weatherData, weatherIconPath }) =
     <div className="flex flex-col self-center sm:self-start mt-40 sm:m-0 justify-center items-center h-auto sm:h-screen sm:w-1/3 w-full">
       <div className="bg-black/25 rounded-lg shadow-lg p-8">
         <h1 className="text-white text-3xl font-light">{weatherData.city}</h1>
-        <Image src={weatherIconPath} width={128} height={128} alt="sun" />
-        <h2 className="text-white/75 text-3xl font-light">
-          {weatherData.temp}°C
-        </h2>
+        <div className='flex'>
+          <Image src={weatherIconPath} width={128} height={128} alt="sun" />
+          <h2 className="text-white/75 text-3xl font-light mt-auto">
+            {weatherData.temp}°C
+          </h2>
+
+        </div>
         <h2 className="text-white/75 text-xl">{weatherData.description}</h2>
         <p className="text-white/75">{weatherData.humidity}% Humidade</p>
         <p className="text-white/75">{weatherData.wind_speedy}</p>
